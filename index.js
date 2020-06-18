b688d7915be05c144fad9057a4d4edb4207af863
ariel <arielgordon123@gmail.com>
2020-06-18 15:45:58 +0300

    change to GET instead of POST

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 const express = require("express");
 const path = require("path");
 const puppeteer = require("puppeteer");
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || "3000";
 
-
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
-
+app.use(bodyParser.json());
 
 async function printPDF(link) {
   const browser = await puppeteer.launch({
     headless: true,
-    args: ["--disable-dev-shm-usage",  "--no-sandbox",
-    "--disable-gpu",],
+    args: ["--disable-dev-shm-usage", "--no-sandbox", "--disable-gpu"],
   });
   const page = await browser.newPage();
   await page.goto(link, { waitUntil: "networkidle0" });
@@ -27,20 +24,21 @@ async function printPDF(link) {
   return pdf;
 }
 
-app.post("/", (req, res) => {
-    if(req.body.link){
-        printPDF(
-           req.body.link
-          ).then((pdf) => {
-            res.set({ "Content-Type": "application/pdf", "Content-Length": pdf.length });
-            res.status(200).send(pdf);
-          });
-    }else{
-        res.status(500).json({error: "error"})
-    }
+app.get("/", (req, res) => {
+  if (req.query.link) {
+    printPDF(req.query.link).then((pdf) => {
+      res.set({
+        "Content-Type": "application/pdf",
+        "Content-Disposition": "attachment; filename=cv.pdf",
+        "Content-Length": pdf.length,
+      });
+      res.end(pdf);
+    });
+  } else {
+    res.status(500).json({ error: "error" });
+  }
 });
 
 app.listen(port, () => {
   console.log(`Listening to requests on http://localhost:${port}`);
 });
-
